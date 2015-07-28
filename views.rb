fb0bce0c55c305ca6855ace8b195081c3b842614
Bluebie the Fox now I guess???! <blueberry@creativepony.com>
2015-07-28 20:28:47 +1000

    fix now invalid JSON gem usage

--- views.rb
+++ views.rb
@@ -14,11 +14,11 @@ module UserInterface::Views
       script :src => "#{@root}/script/compiled?last-modified=#{File.mtime('script').to_i}"
       script do
         text "Cufon.replace('.text');"
-        text "window.urlroot = #{JSON.generate(@root)};"
-        text "window.room = #{JSON.generate(@room)};" if @room
-        text "window.settings = #{JSON.generate(@settings)};" if @settings
-        text "window.openid = #{JSON.generate(@state.identity)};" if @state.identity
-        text "window.smiliesData = #{JSON.generate(@smilies)};" if @smilies
+        text "window.urlroot = #{@root.to_json()};"
+        text "window.room = #{@room.to_json};" if @room
+        text "window.settings = #{@settings.to_json};" if @settings
+        text "window.openid = #{@state.identity.to_json};" if @state.identity
+        text "window.smiliesData = #{@smilies.to_json};" if @smilies
       end
       @headstuff.call if @headstuff
     end
@@ -28,26 +28,26 @@ module UserInterface::Views
     end
     self << "</html>"
   end
-  
+
   def login
     @headstuff = lambda do
       load_scripts 'login.js'
       @headers['X-XRDS-Location'] = URL('/xrds.xml').to_s
     end
-    
+
     regularContainer do
       div.bar.top do
         barTitle("#{AppName} Chats")
       end
-      
+
       p.intro "#{AppName} is a tiny little place to chat. Built to be beautiful and simple. A place for creative people of all styles. Come on in and bring your friends! This peaceful community is dedicated to maintaining a high standard of chatter, so leave your loopholes at the door. If you have an idea on how to make #{AppName} an even better place to be, speak up in the Feedback room once you login!"
-      
+
       div.rule { hr }
-      
+
       div.bar.loginForm! do
         span "Login using your", :class => 'text typeface-js'
         select.idp! { }
-        
+
         span.extra.directedID! { span 'identity', :class => 'text typeface-js' }
         span.extra.IDURL! do
           span 'URL:', :class => 'text typeface-js'
@@ -57,32 +57,32 @@ module UserInterface::Views
           span 'username', :class => 'text typeface-js'
           input.username! :value => ''
         end
-        
+
         button("Continue", :right, :id => 'login')
       end
-      
+
       div.error @error if @error
       noscript { div.error("Javascript is required to use #{AppName}. Enable it in your browser preferences.") }
     end
   end
-  
+
   def home
     @headstuff = proc do
       load_scripts 'home.js' #, 'swiff.uploader.js'
       script "var userDir = '/#{userdir}';"
     end
-    
+
     regularContainer do
       div.bar.top do
         barTitle("Chat Rooms")
         div.lefted { button('Logout', :left, :href => R(Logout)) }
         div.righted { button('New Room', :href => R(CreateRoom)) }
       end
-      
+
       div.scroll do
         div.profile.editable do
           img.ender :src => "#{@root}/style/user-profile-top.png"
-          
+
           a.bigAvatarEdit.userAv! do
             if File.exist?("#{userdir}/avatar-80.png")
               time = File.mtime("#{userdir}/avatar-80.png")
@@ -97,15 +97,15 @@ module UserInterface::Views
               span.removeAvatar! 'Remove Picture'
             end
           end
-          
+
           input.name! :value => @profile['name']
           textarea.desc! @profile['desc']
-          
+
           img.ender.clearer :src => "#{@root}/style/user-profile-bottom.png"
         end
-        
+
         rule
-        
+
         div.rooms do
           @rooms[0...6].each do |room|
             a(:class => "roomButton #{'dead' if room['last-beat'].to_i < Time.now.to_i - 60*10}", :href => room['url']) do
@@ -119,7 +119,7 @@ module UserInterface::Views
             end
           end
         end
-        
+
         if @rooms[6..-1]
           rule
           div.rooms do
@@ -131,24 +131,24 @@ module UserInterface::Views
       end
     end
   end
-  
-  
+
+
   def chat
     @headstuff = proc do
       ui_name = @settings['interface'] || 'chatie'
       ui_root = "#{@root}/UIs/#{ui_name}"
       link :href => "#{ui_root}/styles.css?#{File.mtime("UIs/#{ui_name}/styles.css").to_i}", :rel => 'stylesheet'
-      script "window.ui_root = #{JSON.generate(ui_root + '/')};"
+      script "window.ui_root = #{(ui_root + '/').to_json};"
       script :src => "#{ui_root}/script.js?#{File.mtime("UIs/#{ui_name}/script.js").to_i}"
       link :rel => 'shortcut icon', :href => "#{@root}/#{room_or_default(@room, 'avatar-16.png')}", :type => 'image/png', :sizes => '16x16'
       link :rel => 'shortcut icon', :href => "#{@root}/#{room_or_default(@room, 'avatar-80.png')}", :type => 'image/png', :sizes => '80x80'
       link :rel => 'shorturl', :href => URL(ShortURL, @room).to_s
       link :rel => 'up', :href => URL(Home).to_s
     end
-    
+
     ''
   end
-  
+
   def room_gone
     @status = 410
     regularContainer do
@@ -156,27 +156,27 @@ module UserInterface::Views
         div.lefted { button('Back', :left, :onclick => 'history.go(-1)') }
         div.centered { barTitle('Room Gone') }
       end
-      
+
       div.error "The room ‘#{@room}’ isn’t anywhere on this server. Perhaps the owner deleted it, or perhaps it was removed for lack of use."
     end
   end
-  
+
   def create_room
     @headstuff = proc do
       script 'window.addEvent("load", function() { $("room_id").focus(); });'
       link :rel => 'up', :href => URL(Home).to_s
     end
-    
+
     regularContainer do
       div.bar.top do
         div.lefted { button('Cancel', :left, :href => R(Home)) }
         div.centered { barTitle('Create Room') }
       end
-      
+
       if @error
         div.error @error
       end
-      
+
       div.bigBlock do
         img.ender :src => "#{@root}/style/user-profile-top.png"
         div.body do
@@ -193,33 +193,33 @@ module UserInterface::Views
       end
     end
   end
-  
+
   def room_settings
     @headstuff = proc do
       load_scripts 'settings.js'
-      script "window.room = #{JSON.generate(@room)};"
+      script "window.room = #{@room.to_json};"
       link :rel => 'up', :href => URL(Room, @room).to_s
     end
-    
+
     regularContainer('settingsBody') do
       div.bar.top do
         div.lefted { button('Cancel', :left, :href => R(Room, @room)) }
         div.centered { barTitle('Room Settings') }
         div.righted { button('Save', :onclick => '$("settingsForm").submit();') }
       end
-      
-      
+
+
       div.error(@error) if @error
-      
+
       form.settingsForm!.bigBlock(:method => 'post') do
         img.ender :src => "#{@root}/style/user-profile-top.png"
         dl.body do
           dt 'Room Name:'
           dd { input.title! :value => @settings['title'] }
-          
+
           dt 'Description:'
           dd { textarea.desc! @settings['desc'] }
-          
+
           dt 'Room Interface:'
           dd do
             select.interface! do
@@ -229,7 +229,7 @@ module UserInterface::Views
               end
             end
           end
-          
+
           dt 'Icon:'
           dd do
             a.bigAvatarEdit.roomAv! do
@@ -243,25 +243,25 @@ module UserInterface::Views
               end
             end
           end
-          
+
           dd.gap { hr }
-          
+
           @settings['background'] ||= {} # incase it isn't there
-          
+
           dt 'Use Custom Background?'
           dd { input.background_enabled!({:type => 'checkbox'}.merge(@settings['background']['enabled']?{:checked=>'checked'}:{})) }
-          
+
           dt 'Background Colour:'
           dd { input.colour.background_colour! :value => @settings['background']['colour'] || '#8FB359' }
-          
+
           dt 'Background Image URL:'
           dd { input.url.background_url! :value => @settings['background']['url'] || '' }
-          
+
           dt 'Repeat Background Image?'
           dd { input.repeat_background!({:type => 'checkbox'}.merge((@settings['background']['repeat'] || true)?{:checked=>'checked'}:{})) }
-          
+
           dd.gap { hr }
-          
+
           dt 'Gagged Users'
           dd do
             input.gagged! :type => 'hidden', :value => (@settings['gagged'] || []).join("\n")
@@ -272,19 +272,19 @@ module UserInterface::Views
               button 'Add Gag', :id => 'add_gag_btn'
             end
           end
-          
+
           dd.gap { hr }
-          
+
           @settings['bot'] ||= {'enabled' => false, 'url' => ''}
-          
+
           dt 'Enable Robot:'
           dd { input.bot_enabled!({:type => 'checkbox'}.merge(@settings['bot']['enabled']?{:checked=>'checked'}:{})) }
-          
+
           dt 'Robot URL:'
           dd { input.url.bot_url! :value => @settings['bot']['url'] }
-          
+
           dd.gap { hr }
-          
+
           dt 'Erase Data:'
           dd do
             div { button('Remove All Messages', :href => R(EraseRoom, @room)) }
@@ -295,41 +295,41 @@ module UserInterface::Views
       end
     end
   end
-  
+
   def generic_error(title, message)
     regularContainer {
       div.bar.top { div.lefted { button('Back', :left, :onclick => 'history.go(-1)') }; barTitle(title || "error") }
       p.intro message.to_s
     }
   end
-  
-  
+
+
   private
-  
+
   def load_scripts(*scripts)
     scripts.each do |script|
       script(:src => "#{@root}/script/#{script}?modified=#{File.mtime('script/'+script).to_i}")
     end
   end
-  
+
   def regularContainer(classy = 'regularBody body', &content)
     div.regularContainer.container { div(:class => classy, &content); div.footer {} }
   end
-  
+
   def barTitle(text)
     @title = text
     div.title { span.text(text) }
   end
-  
+
   # second argument can be a direction, in symbol form, left, right, or square
   def button(text, *args)
     direction = args.first.is_a?(Symbol) ? args.shift : :square
     attributes = args.shift || {}
     a(attributes.merge(:class => "button #{direction}")) { em {}; text text }
   end
-  
+
   def rule; div.rule { hr }; end
-  
+
   def sidebarBoxy(*attribs, &inside)
     div.boxy(*attribs) do
       img.ender.top :src => "#{@root}/style/sidebar-rect-top.png"
@@ -337,7 +337,7 @@ module UserInterface::Views
       img.ender.bottom :src => "#{@root}/style/sidebar-rect-bottom.png"
     end
   end
-  
+
   def little_favatar_link(room)
     a(:href => R(Room, room['id']), :class => "little_av #{'dead' if room['last-beat'] < Time.now - 60*10}") do
       av_path = room_or_default(room['id'], 'avatar-16.png')
